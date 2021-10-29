50995ef162b60a27191747158c8209d478c84bd6
Jose Armando Meza Vigueras <jamv.vigueras@gmail.com>
2021-10-29 12:33:42 -0500

    Primera base de datos

---
+++ Marvel/php/db.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE Tiendas_Comics;
+
+CREATE TABLE Sucursales(
+    id_Sucursal bigint auto_increment,
+    sNombre VARCHAR(40),
+    sDireccion VARCHAR(100),
+    iTelefono BIGINT,
+    id_Usuario BIGINT,
+
+    PRIMARY KEY (id_Sucursal)
+);
+
+CREATE TABLE Usuarios(
+    id_Usuarios BIGINT auto_increment,
+    sNombre VARCHAR(40),
+    sEmail VARCHAR(25),
+    sPassword VARCHAR(16),
+    iType INT,
+
+    PRIMARY KEY(id_Usuarios)
+);
+
+CREATE TABLE Inventario(
+    id_Comic BIGINT auto_increment,
+    iCantidad INT,
+    id_Sucursal BIGINT
+);
