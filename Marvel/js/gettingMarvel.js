98044df7998c9987e3b4fcb2e7fe016b1cb66c93
Carlos Enrique Osorio Cid del Prado <carlososorio81@hotmail.com>
2021-11-01 14:35:22 -0600

    Push

--- Marvel/js/gettingMarvel.js
+++ Marvel/js/gettingMarvel.js
@@ -11,6 +11,17 @@ const marvel = {
         const urlAPI = 'https://gateway.marvel.com:443/v1/public/comics?ts=9&apikey=e92d9a401a0300d4b9e80b0964d9ef16&hash=0e187be3348823a006ef2076ac1d7293';
         const container = document.querySelector('#marvel-comics-row');
         let contentHTML = '';
+        
+        class structComics {
+            constructor(s_comicId, s_comicName, s_comicCover) {
+                this.s_comicId = s_comicId;
+                this.s_comicName = s_comicName;
+                this.s_comicCover = s_comicCover;
+            }
+        }
+        
+        var s_structComics = [];
+
 
         fetch(urlAPI)
             .then(res => res.json())
@@ -25,16 +36,28 @@ const marvel = {
 
                     // console.log(comicCover);
                     
+                    s_structComics.push(new structComics(comicId, comicName, comicCover));
+                }
 
+                s_structComics.sort(function(a,b){
+                    if(a.s_comicName < b.s_comicName) {return -1;}
+                    if(a.s_comicName > b.s_comicName) {return 1;}
+                    return 0;
+                })
+                
+               //console.log(s_structComics.length);
+
+                for(const comic of json.data.results){
+                 
                     if(i % 4 == 0){ contentHTML += "<tr>"; }
 
                     contentHTML += "<td>";
                     contentHTML += "    <div class='md-col-4'>";
-                    contentHTML += "        <center><a href='singleComic.php?id="+comicId+"'>";
-                    contentHTML += "            <img src='"+comicCover+"' alt='' class='img-thumbnail'>";
+                    contentHTML += "        <center><a href='singleComic.php?id="+s_structComics[i].s_comicId+"'>";
+                    contentHTML += "            <img src='"+s_structComics[i].s_comicCover+"' alt='' class='img-thumbnail'>";
                     contentHTML += "        </a></center>";
                     contentHTML += "        <br>";
-                    contentHTML += "        <h5 class='title'>"+comicName+"</h5>";
+                    contentHTML += "        <h5 class='title'>"+s_structComics[i].s_comicName+"</h5>";
                     contentHTML += "    </div>";
                     contentHTML += "<td>";
 
